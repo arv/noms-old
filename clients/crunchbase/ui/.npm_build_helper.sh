5db262dd9d2a2826b80863a43334497adf69caed
Erik Arvidsson <erik.arvidsson@gmail.com>
2015-12-11 11:33:13 -0500

    CB dev mode

--- clients/crunchbase/ui/.npm_build_helper.sh
+++ clients/crunchbase/ui/.npm_build_helper.sh
@@ -2,12 +2,27 @@
 SRC="src/main.js"
 OUT="out.js"
 
-export NODE_ENV=production
-export BABEL_ENV=production
+NOMS_ROOT="../../.."
+NOMS_SRC="$NOMS_ROOT/js/src"
+ROLLUP_CMD="node_modules/.bin/rollup --config --input $SRC"
 
-cp node_modules/nvd3/build/nv.d3.min.css nvd3.css
-cp node_modules/nvd3/build/nv.d3.min.js nvd3.js
-cp node_modules/d3/d3.min.js d3.js
+export NODE_ENV=$1
+export BABEL_ENV=$1
 
-node_modules/.bin/rollup --config --input $SRC \
-    | node_modules/.bin/uglifyjs -c warnings=false -m -w --screw-ie8 > $OUT
+if [ $1 == "production" ]; then
+  cp node_modules/nvd3/build/nv.d3.min.css nvd3.css
+  cp node_modules/nvd3/build/nv.d3.min.js nvd3.js
+  cp node_modules/d3/d3.min.js d3.js
+
+  $ROLLUP_CMD | node_modules/.bin/uglifyjs -c warnings=false -m -w --screw-ie8 > $OUT
+elif [ $1 == "development" ]; then
+  cp node_modules/nvd3/build/nv.d3.css nvd3.css
+  cp node_modules/nvd3/build/nv.d3.js nvd3.js
+  cp node_modules/d3/d3.js d3.js
+
+  ROLLUP_CMD="$ROLLUP_CMD --sourcemap inline"
+
+  node_modules/.bin/http-server &
+  node_modules/.bin/nodemon --watch . --watch $NOMS_SRC --ignore $OUT \
+      -x "$ROLLUP_CMD > $OUT"
+fi
--- clients/splore/.npm_build_helper.sh
+++ clients/splore/.npm_build_helper.sh
@@ -2,7 +2,8 @@
 SRC="main.js"
 OUT="out.js"
 
-NOMS_SRC="../../js/src"
+NOMS_ROOT="../.."
+NOMS_SRC="$NOMS_ROOT/js/src"
 ROLLUP_CMD="node_modules/.bin/rollup --config --input $SRC"
 
 export NODE_ENV=$1
@@ -11,6 +12,8 @@ export BABEL_ENV=$1
 if [ $1 == "production" ]; then
   $ROLLUP_CMD | node_modules/.bin/uglifyjs -c warnings=false -m -w --screw-ie8 > $OUT
 elif [ $1 == "development" ]; then
+  ROLLUP_CMD="$ROLLUP_CMD --sourcemap inline"
+
   node_modules/.bin/http-server &
   node_modules/.bin/nodemon --watch . --watch $NOMS_SRC --ignore $OUT \
       -x "$ROLLUP_CMD > $OUT"
